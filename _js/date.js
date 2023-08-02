ce012217cb248517e86c5c4ae5100d0e4c4075fc
Angelo <angelofernandez15@outlook.fr>
2023-08-02 12:44:58 +0200

    test

---
+++ _js/date.js
@@ -0,0 +1,3 @@
+"use strict;"
+
+console.log("le fichier date est chargé ")
---
+++ _js/misc.js
@@ -0,0 +1,18 @@
+"use strict;"
+
+console.log("le fichier misc est chargé ")
+
+function sayMessage(msg)
+{
+    console.log(msg);
+}
+
+function sayHello()
+{
+    sayMessage("Début du programme 'Deviner un Nombre'");
+}
+
+function sayGoodbye()
+{
+    sayMessage("Fin du programme 'Deviner un Nombre'");
+}
